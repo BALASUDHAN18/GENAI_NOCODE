7397d8af72c5227f5e5d6581baf72311a609000f
BALASUDHAN18 <balasudhan301039@gmail.com>
2025-03-07 08:53:47 +0530

    success

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Header.css';
+import Logo from '../../assets/Logos/GENERATIVE_AI.svg'
+import Search from '../../assets/Icons/Search.svg'
+const Header = () => {
+  return (
+    <div className='Header__Container'>
+      <div className='Header__Left'>
+        <img src={Logo} alt="Generative_AI" />
+      </div>
+      <div className='Header__Middle'>
+    <input className="Header__input" placeholder="What do you want to automate?" />
+    <img src={Search} alt="Search-Icon" />
+      </div>
+      <div className='Header__Right'>
+        <button className='Header__Signup'>
+            Signup
+        </button>
+        <button className='Header__Signin'>
+            Signin
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './NavBar.css';
+import Home from '../../assets/NavbarIcons/Home.svg'
+import Circle from '../../assets/NavbarIcons/Add_Circle.svg'
+import Notes from '../../assets/NavbarIcons/Notes.svg'
+import Settings from '../../assets/NavbarIcons/Settings.svg'
+import Timeline from '../../assets/NavbarIcons/Timeline.svg'
+const NavBar = () => {
+  return (
+    <div className='navBar__container'>
+    <div className='navBar__elements'>
+    <button>
+      <img src={Home} alt="Home" />
+    </button>
+    <button>
+      <img src={Circle} alt="Add_Circle" />
+    </button>
+    <button>
+      <img src={Notes} alt="Notes" />
+    </button>
+    <button>
+      <img src={Settings} alt="Settings" />
+    </button>
+    <button>
+      <img src={Timeline} alt="Timeline" />
+    </button>
+    </div>
+    </div>
+  )
+}
+
+export default NavBar
