4e38563a3643c02154ba0ce23c2885e1698728de
JudeSamJ <judesamuelsjj@gmail.com>
2025-03-08 23:54:33 +0530

    success

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,32 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './NavBar.css';
-import Home from '../../assets/NavbarIcons/Home.svg'
-import Circle from '../../assets/NavbarIcons/Add_Circle.svg'
-import Notes from '../../assets/NavbarIcons/Notes.svg'
-import Settings from '../../assets/NavbarIcons/Settings.svg'
-import Timeline from '../../assets/NavbarIcons/Timeline.svg'
+import Home from '../../assets/NavbarIcons/Home.svg';
+import Circle from '../../assets/NavbarIcons/Add_Circle.svg';
+import Notes from '../../assets/NavbarIcons/Notes.svg';
+import Settings from '../../assets/NavbarIcons/Settings.svg';
+import Timeline from '../../assets/NavbarIcons/Timeline.svg';
+
 const NavBar = () => {
+  const [isCircleOpen, setIsCircleOpen] = useState(false);
+
   return (
     <div className='navBar__container'>
-    <div className='navBar__elements'>
-    <button>
-      <img src={Home} alt="Home" />
-    </button>
-    <button>
-      <img src={Circle} alt="Add_Circle" />
-    </button>
-    <button>
-      <img src={Notes} alt="Notes" />
-    </button>
-    <button>
-      <img src={Settings} alt="Settings" />
-    </button>
-    <button>
-      <img src={Timeline} alt="Timeline" />
-    </button>
-    </div>
-    </div>
-  )
-}
+      <div className='navBar__elements'>
+        <button>
+          <img src={Home} alt='Home' />
+        </button>
+        <div
+          className='dropdown-container'
+          onMouseEnter={() => setIsCircleOpen(true)}
+          onMouseLeave={() => setIsCircleOpen(false)}
+        >
+          <button className='dropdown-button'>
+            <img src={Circle} alt='Add Circle' />
+          </button>
 
-export default NavBar
+          {isCircleOpen && (
+            <div className='dropdown-menu'>
+              <a href='#'>New Task</a>
+              <a href='#'>New Event</a>
+              <a href='#'>New Note</a>
+            </div>
+          )}
+        </div>
+
+        <button>
+          <img src={Notes} alt='Notes' />
+        </button>
+        <button>
+          <img src={Settings} alt='Settings' />
+        </button>
+        <button>
+          <img src={Timeline} alt='Timeline' />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
