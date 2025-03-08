b4c90f16acb90bcd00013e4fa082477bd962eb50
kiran2004c@gmail.com <kiran2004c@gmail.com>
2025-03-08 23:38:38 +0530

    success

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -5,6 +5,7 @@ import Circle from '../../assets/NavbarIcons/Add_Circle.svg'
 import Notes from '../../assets/NavbarIcons/Notes.svg'
 import Settings from '../../assets/NavbarIcons/Settings.svg'
 import Timeline from '../../assets/NavbarIcons/Timeline.svg'
+import {Link} from 'react-router-dom'
 const NavBar = () => {
   return (
     <div className='navBar__container'>
@@ -12,9 +13,10 @@ const NavBar = () => {
     <button>
       <img src={Home} alt="Home" />
     </button>
+    <Link to={`workflow`}>
     <button>
       <img src={Circle} alt="Add_Circle" />
-    </button>
+    </button></Link>
     <button>
       <img src={Notes} alt="Notes" />
     </button>
